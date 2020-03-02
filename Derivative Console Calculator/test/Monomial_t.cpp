e21e13ffbd59194c0b1c2913bcc356798f4b211f
jrf115 <jrf115@zips.uakron.edu>
2020-03-02 11:24:27 -0500

    Refactor Monomial_t.cpp

--- Derivative Console Calculator/test/Monomial_t.cpp
+++ Derivative Console Calculator/test/Monomial_t.cpp
@@ -10,27 +10,27 @@
 
 namespace {
 
-TEST(monomialsConstruct, empty) {
-	Monomial m;
-	EXPECT_EQ(m.getCoef(), 0);
-	EXPECT_EQ(m.getTerm(), "");
-}
+	TEST(monomialsConstruct, empty) {
+		Monomial m;
+		EXPECT_EQ(m.getCoef(), 0);
+		EXPECT_EQ(m.getTerm(), "");
+	}
 
-TEST(monomialsConstruct, 2x) {
-	Monomial m(2, "x");
-	EXPECT_EQ(m.getCoef(), 2);
-	EXPECT_EQ(m.getTerm(), "x");
-}
+	TEST(monomialsConstruct, 2x) {
+		Monomial m(2, "x");
+		EXPECT_EQ(m.getCoef(), 2);
+		EXPECT_EQ(m.getTerm(), "x");
+	}
 
-TEST(monomialsConstruct, 2xEXP3) {
-	Monomial m(2, "x^3");
-	EXPECT_EQ(m.getCoef(), 2);
-	EXPECT_EQ(m.getTerm(), "x^3");
-}
+	TEST(monomialsConstruct, 2xEXP3) {
+		Monomial m(2, "x^3");
+		EXPECT_EQ(m.getCoef(), 2);
+		EXPECT_EQ(m.getTerm(), "x^3");
+	}
 
-TEST(monomialsConstruct, mEqualsm) {
-	Monomial m(1, "x");
-	EXPECT_TRUE(m == m);
-}
+	TEST(monomialsConstruct, mEqualsm) {
+		Monomial m(1, "x");
+		EXPECT_TRUE(m == m);
+	}
 
 } // namespace
