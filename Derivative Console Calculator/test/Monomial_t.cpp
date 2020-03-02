0d0078032a09053afdd335a77884fbb522c74c0c
jrf115 <jrf115@zips.uakron.edu>
2020-03-02 11:19:32 -0500

    Refactor Monomial construct cases

--- Derivative Console Calculator/test/Monomial_t.cpp
+++ Derivative Console Calculator/test/Monomial_t.cpp
@@ -8,25 +8,29 @@
 #include "gtest/gtest.h"
 
 
-TEST(monomials, empty) {
+namespace {
+
+TEST(monomialsConstruct, empty) {
 	Monomial m;
 	EXPECT_EQ(m.getCoef(), 0);
 	EXPECT_EQ(m.getTerm(), "");
-};
+}
 
-TEST(monomials, 2x) {
+TEST(monomialsConstruct, 2x) {
 	Monomial m(2, "x");
 	EXPECT_EQ(m.getCoef(), 2);
 	EXPECT_EQ(m.getTerm(), "x");
 }
 
-TEST(monomials, 2xEXP3) {
+TEST(monomialsConstruct, 2xEXP3) {
 	Monomial m(2, "x^3");
 	EXPECT_EQ(m.getCoef(), 2);
 	EXPECT_EQ(m.getTerm(), "x^3");
 }
 
-TEST(monomials, mEqualsm) {
+TEST(monomialsConstruct, mEqualsm) {
 	Monomial m(1, "x");
-	EXPECT_EQ((m == m), true);
+	EXPECT_TRUE(m == m);
 }
+
+} // namespace
