4e49899ac35c8ccb3414ec666067b40dc3eaeae3
jrf115 <jrf115@zips.uakron.edu>
2020-03-02 12:19:18 -0500

    Add poly_t case to namespace and add f(x) = 2x test case

--- Derivative Console Calculator/test/Polynomial_t.cpp
+++ Derivative Console Calculator/test/Polynomial_t.cpp
@@ -9,8 +9,21 @@
 #include "gtest/gtest.h"
  
 
-TEST(polynomialsConstruct, empty) {
-	Polynomial f;
-	EXPECT_TRUE(f.getPolyn().empty());
+namespace {
+
+	TEST(polynomialsConstruct, empty) {
+		Polynomial f;
+		EXPECT_TRUE(f.getPolyn().empty());
+	};
+
+	/* f(x) = 2x */
+	TEST(polynomialsConstruct, TWOx) {
+		Polynomial f;
+		string s("2x");
+		stringstream iss(s), oss;
+		iss >> f;
+		oss << f;
+		EXPECT_EQ(iss.str(), oss.str());
+	};
 
 }
