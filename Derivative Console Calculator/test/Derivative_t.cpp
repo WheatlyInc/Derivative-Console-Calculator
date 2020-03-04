65b3a8e97c5cdb10f4063c60894f385d2166182c
jrf115 <jrf115@zips.uakron.edu>
2020-03-04 13:14:21 -0500

    Add Derivative_t.cpp

---
+++ Derivative Console Calculator/test/Derivative_t.cpp
@@ -0,0 +1,15 @@
+/**
+* Derivative_t.cpp
+* A testing file to test class Derviative
+*
+**/
+
+#include "../inc/internal/Derivative.h"
+#include "../extern/googletest/googletest/include/gtest/gtest.h"
+
+
+namespace {
+
+
+
+}
